d2c32d1b5fa6820c0ed2842dbe0a5f676c9cd96d
Kingtous <kingtous@qq.com>
2019-01-13 15:01:33 +0800

    fix bugs

---
+++ src/old_sbb.py
@@ -0,0 +1,308 @@
+# getBasicBlockSlice
+import os
+import re as r #正则表达式库
+import sys
+def getBasicBlockSlice(list):
+        '''
+        负责人：覃辉，苏文辉
+        目的：将大BB转为一个个小BB，存成一个list
+        ( 碰到label元素，将label修改成returnBB对应的label，所以return的label段需要在list中特殊标明，方便易找)
+        # return 要求：切出list对应的大BB的每一个小BB（按每个label为元素），按上述要求改进元素，并将这些元素组成一个list，返回。
+
+        未完成，bug已经清除，还差跳转功能
+        '''
+        count=1
+        for bb in list:
+            dict = {}  # 创建字典
+            start_flag = 'return'
+            return_start_place = bb.find(start_flag)
+            s=bb[:return_start_place-1]
+            s1=s[::-1]
+            return_start_pl=s1.find('lebal')
+            for j in range(return_start_pl,len(s1)):
+                if s1[j] == '{':
+                    return_start_pl=j
+                    break
+            return_start_place=len(s)-len(s1[:return_start_pl])-1
+            # for j in range(0,return_start_place):
+            #     if bb[return_start_place-j] == '{':
+            #         return_start_place=return_start_place-j
+            #         print(bb[return_start_place-4:return_start_place+4])
+            #         print(count)
+            #         count+=1
+            #         break
+            # return_start_place = r.search('{\s*label\s*\d*\s*{\s*lref\s*\d*\s*"\w*::bb\d*"\s*}\s*{\s*dec_unsigned\s*\d*\s*\d\s*}\s*}\s*{\s*return',bb).start()
+            # return 语句开始部分
+            return_end_pl=bb.find('return',return_start_place)
+            num=1
+            for i in range(return_end_pl+5,len(bb)):
+                if bb[i]=='{':
+                    num+=1
+                elif bb[i]=='}':
+                    num-=1
+                    if num==-1:
+                        return_end_place=i
+                        break
+            # return_end_place = r.search('{\s*return\s*}', bb).end()
+            # return语句结束部分
+            dict['return'] = bb[return_start_place:return_end_place]
+            # print(bb[return_start_place:return_end_place])
+            # 将return语句写入字典
+            flag = 1  # 无限循环标志
+
+            temp = ' '  # 临时字符串
+            bb_start_place_flag = r.search('{\s*label\s*\d*\s*{\s*lref\s*\d*\s*"\w*::bb\d*"\s*}\s*{\s*dec_unsigned\s*\d*\s*\d\s*}\s*}', bb)
+            # print(bb_start_place_flag)
+            # if(bb_start_place_flag != None):
+            #     print("yes")
+            #
+            # else:
+            #     print("no")
+            bb = bb[:return_start_place]
+            # print(bb)
+            if (bb_start_place_flag != None):
+
+                while (flag):
+                    bb_start_place_flag = r.search('{\s*label\s*\d*\s*{\s*lref\s*\d*\s*"\w*::bb\d*"\s*}\s*{\s*dec_unsigned\s*\d*\s*\d\s*}\s*}', bb)
+                    # 预先搜索bb label 开始部分
+                    bb_stop_place_flag = r.search( '{\s*jump\s*{\s*label\s*\d*\s*{\s*lref\s*\d*\s*"\w*::bb\d*"\s*}\s*{\s*dec_unsigned\s*\d*\s*\d*\s*}\s*}\s*leaving\s*\d*\s*}',
+                        bb)
+                    # 预先搜索bb label 结束部分
+                    default_end_place_flag = r.search('{\s*default\s*{\s*label\s*64\s*{\s*lref\s*\d*\s*"\w*::bb\d*"\s*}\s*{\s*dec_unsigned\s*\d*\s*\d\s*}\s*}\s*}\s*}',bb)
+                    # 预先搜索bb 中 default 结束部分
+                    if (bb_stop_place_flag == None and default_end_place_flag == None):
+                        # print("1"+bb)
+                        # print("1")
+                        if (bb.find('label')==-1):
+                            break
+                        else:
+                            bb_start_place =bb.find('label')
+                            bb_st_place=0
+                            for i in range(0,bb_start_place):
+                                if bb[bb_start_place-i]=='{':
+                                    bb_st_place=bb_start_place-i
+                                    break
+
+                            for i in range(0,len(bb)):
+                                if bb[i]=='}':
+                                    bb_en_place=i
+                            temp = bb[bb_st_place:bb_en_place+1]
+                            dict[r.search('bb\d*', temp).group()] = temp
+                            break
+                    # 如果再也搜不到则退出循环
+                    if (bb_start_place_flag != None):
+                        bb_start_place = r.search(
+                            '{\s*label\s*\d*\s*{\s*lref\s*\d*\s*"\w*::bb\d*"\s*}\s*{\s*dec_unsigned\s*\d*\s*\d\s*}\s*}',
+                            bb).start()
+                    if (bb_stop_place_flag != None):
+                        bb_stop_place = r.search(
+                            '{\s*jump\s*{\s*label\s*\d*\s*{\s*lref\s*\d*\s*"\w*::bb\d*"\s*}\s*{\s*dec_unsigned\s*\d*\s*\d*\s*}\s*}\s*leaving\s*\d*\s*}',bb).end()
+                    if (default_end_place_flag != None):
+                        default_end_place = r.search(
+                            '{\s*default\s*{\s*label\s*64\s*{\s*lref\s*\d*\s*"\w*::bb\d*"\s*}\s*{\s*dec_unsigned\s*\d*\s*\d\s*}\s*}\s*}\s*}',bb).end()
+                    # 判断是否搜得到，搜得到返回索引
+                    # 判断是jump语句先出现还是default语句先出现
+                    if (default_end_place_flag!=None and default_end_place < bb_stop_place):
+                        # print("2")
+                        temp = bb[bb_start_place:default_end_place]
+                        dict[(r.search('bb\d*', temp)).group()] = temp
+                        start=default_end_place
+                        end=return_start_place
+                        bb = bb[start:end]
+                        #start #切片开始位置索引
+                        #end #切片结束位置索引
+                        # bb语句缩短，从搜到的位置重新开始
+                        continue
+
+
+                    else:
+                        # print("3")
+                        temp = bb[bb_start_place:bb_stop_place]
+                        # print(temp)
+                        try:
+                            dict[r.search('bb\d*', temp).group()] = temp
+                        except:
+                            print('error: '+bb)
+                            print(bb[bb_start_place:])
+                            print(bb[bb_stop_place:])
+                        start=bb_stop_place
+                        end=return_start_place
+                        bb = bb[start:end]
+                        continue
+        # for value in dict.values():
+        #     string=value
+        #     label_l='"'b
+        #     start_place=string.find(label_l)
+        #     end_place=string.find(label_l,start_place+1)
+        #     label=string[start_place+1:end_place]
+        #     b_start_place = r.search('{\s*label\s*\d*\s*{\s*lref\s*\d*\s*"\w*::bb\d*"\s*}\s*{\s*dec_unsigned\s*\d*\s*\d\s*}\s*}',string).end()
+            # stringl=string[b_start_place:]
+        #     b_start_place = r.search('{\s*label\s*\d*\s*{\s*lref\s*\d*\s*"\w*::bb\d*"\s*}\s*{\s*dec_unsigned\s*\d*\s*\d\s*}\s*}',string[b_start_place:]).start()
+        #     count=0
+        #     while b_start_place!=-1:
+        #         start_place=string.find(label,end_place+1)
+        #         if string[start_place+len(label)]==':':
+        #             bb_old=string[0:b_start_place]
+        #             dict[(r.search('bb\d*'+':'+count, bb_old)).group()] = bb_old
+        #             count+=1
+        #             string=string[b_start_place:]
+        #             b_start_place = r.search('{\s*label\s*\d*\s*{\s*lref\s*\d*\s*"\w*::bb\d*"\s*}\s*{\s*dec_unsigned\s*\d*\s*\d\s*}\s*}',string).end()
+        #             b_start_place = r.search('{\s*label\s*\d*\s*{\s*lref\s*\d*\s*"\w*::bb\d*"\s*}\s*{\s*dec_unsigned\s*\d*\s*\d\s*}\s*}',string[b_start_place:]).start()
+        #         else:
+        #             string=string[b_start_place:]
+        #             b_start_place = r.search('{\s*label\s*\d*\s*{\s*lref\s*\d*\s*"\w*::bb\d*"\s*}\s*{\s*dec_unsigned\s*\d*\s*\d\s*}\s*}',string).end()
+        #             b_start_place = r.search('{\s*label\s*\d*\s*{\s*lref\s*\d*\s*"\w*::bb\d*"\s*}\s*{\s*dec_unsigned\s*\d*\s*\d\s*}\s*}',string[b_start_place:]).start()
+        new_dict={}
+        # print(dict)
+        # dict={'bb':string}
+        for key in dict:
+            # print(dict[key])
+            string=dict[key]
+            count=0
+            # temp_cout=0
+            #print(key)
+            even_place=0
+            label_l='"'
+            start_place=string.find(label_l)
+            end_place=string.find(label_l,start_place+1)
+            label=string[start_place+1:end_place]
+            end_place=0
+            start_place=string.find(label)
+            # print(label)
+            start_place=string.find(label,start_place+len(label))
+            while start_place!=-1:
+                if string[start_place+len(label)]==':':
+                    # print(temp_cout)
+                    # temp_cout+=1
+                    number=0
+                    for i in range(0,start_place):
+                        if string[start_place-i]=='{':
+                            number+=1
+                            if number==2:
+                                end_place=start_place-i
+                                break
+
+                    if count==0:
+                        new_dict[key] = string[even_place:end_place - 1]
+                        # print(key)
+                    else:
+                        filename_place=string.find('::',even_place)
+                        # print(filename_place)
+                        filename_placel = string.find('::',filename_place+1)
+                        # print(filename_placel)
+                        filename_place_end = string.find('"', filename_placel + 1)
+                        filename_sec =string[filename_placel+2:filename_place_end]
+                        # print(filename_sec)
+                        new_dict[key+'_'+filename_sec] = string[even_place:end_place-1]
+                        # /print(key+'_'+filename_sec)
+                    # new_dict[key+'_'+str(count)] = string[even_place:end_place-1]
+                    # print(even_place)
+                    # print(end_place-1)
+                    count+=1
+                    # print(string[even_place:end_place-1])
+                    even_place=end_place
+                    # pint('\n')
+                    start_place=string.find(label,start_place+len(label))
+                else:
+                    start_place=string.find(label,start_place+len(label))
+            if count!=0:
+                filename_place=string.find('::',even_place)
+                # print(filename_place)
+                filename_placel = string.find('::',filename_place+1)
+                # print(filename_placel)
+                filename_place_end = string.find('"', filename_placel + 1)
+                filename_sec =string[filename_placel+2:filename_place_end]
+                # print(filename_sec)
+                new_dict[key+'_'+filename_sec] = string[end_place:]
+                # //print(key+'_'+filename_sec)
+            else:
+                new_dict[key] = string[end_place:]
+            # print (string[end_place:])
+
+            # while start_place!=-1:
+            #     #print(start_place)
+            #     for s in range(start_place,len(string)):
+            #         if string[s]=="}":
+            #             end_flag_place=s
+            #             break
+            #
+            #     flag_place=string[start_place:end_flag_place].find(label)
+            #     if (flag_place!=-1):
+            #         #print(flag_place)
+            #         # if(string[flag_place+10:flag_place].find("call")==-1):
+            #         if string[flag_place+len(label)]==':':
+            #             #print(string[flag_place+len(label)])
+            #             for i in range(0,start_place):
+            #                 if string[start_place-i]=='{':
+            #                     end_place=start_place-i
+            #
+            #                     break
+            #             new_dict[key+'_'+str(count)] = string[even_place:end_place-1]
+            #             count+=1
+            #             # print(string[even_place:end_place-1])
+            #             even_place=end_place
+            #             # pint('\n')
+            #             start_place=string.find('label',start_place+len('label'))
+            #         else:
+            #             start_place=string.find('label',start_place+len('label'))
+            #     else:
+            #         start_place=string.find('label',start_place+len('label'))
+            #
+            #
+                # else:
+                #     print("___________________________________________")
+                #     start_place=string.find('label',start_place+len('label'))
+
+            # if count!=0:
+            #     new_dict[key+'_'+str(count)] = string[end_place:]
+            # else:
+            #     new_dict[key] = string[end_place:]
+            #     #print(start_place)
+            #     flag_place_start=string.find(label_l,start_place)
+            #     flag_place_end=string.find(label_l,flag_place_start+1)
+            #     flag_label=string[flag_place_start+1:flag_place_end]
+            #
+            #     if flag_label==label:
+            #         #print(flag_place)
+            #         if string[flag_place_start+len(label)]==':':
+            #             #print(string[flag_place+len(label)])
+            #             for i in range(0,start_place):
+            #                 if string[start_place-i]=='{':
+            #                     end_place=start_place-i
+            #
+            #                     break
+            #             new_dict[key+'_'+str(count)] = string[even_place:end_place-1]
+            #             count+=1
+            #             # print(string[even_place:end_place-1])
+            #             even_place=end_place
+            #             # print('\n')
+            #             start_place=string.find('label',start_place+len('label'))
+            #         else:
+            #             start_place=string.find('label',start_place+len('label'))
+            #     elif flag_label!=label:
+            #         start_place=string.find('label',flag_place_end+len('label'))
+            # if count!=0:
+            #     new_dict[key+'_'+str(count)] = string[end_place:]
+            # else:
+            #     new_dict[key] = string[end_place:]
+            # # print (string[end_place:])
+        #file = open('example.alf','w')
+        #for ebb in dict:
+        #    file.writelines(ebb+'\n')
+        #    file.writelines(dict[ebb]+'\n')
+        #file.close()
+        return new_dict
+# print (getBasicBlockSlice(li))
+#for bb in list:
+#    print(bb+'\n')
+# lis=li[0]
+# print (getBasicBlockSlice(lis))
+# for i in range(0,len(li)):
+#         lis=[]
+#         lis.append(li[i])
+#         print(getBasicBlockSlice(lis))
+
+
+
+# for key,value in getBasicBlockSlice(li).items():
+    # print('{key}:{value}'.format(key = key, value = value))
